20de2bcc40210fd193e7016ee4f22112eb017ca6
Martijn Comans <m.r.comans@student.tudelft.nl>
2020-12-02 15:15:44 +0100

    Add net worth visualisation page to app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <NetWorthVisualisationPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NetWorthVisualisationPage from './components/NetWorthVisualisationPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NetWorthVisualisationPage
   }
 }
 </script>
---
+++ src/components/NetWorthVisualisationPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-4xl">Net worth per province</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NetWorthVisualisationPage',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
