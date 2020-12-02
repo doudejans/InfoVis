031341d687429bff747446e539187ab8196e6193
Martijn Comans <m.r.comans@student.tudelft.nl>
2020-12-02 16:41:07 +0100

    Render map with dutch regions

--- src/components/NetWorthMap.vue
+++ src/components/NetWorthMap.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import * as d3 from "d3";
+
 export default {
     name: 'NetWorthMap',
     props: {
@@ -10,7 +12,45 @@ export default {
     data() {
         return {
         }
+    },
+    mounted() {
+        console.log('hallo');
+        const width = 960,
+            height = 1160;
+
+        const svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        d3.json("gemeente_2020.geojson").then((regions) => {
+             const projection = d3.geoMercator()
+                 .scale(10000)
+                 .center([0, 52])
+                 .rotate([-4.8, 0])
+                 .translate([width/2, height/2]);
+
+            const path = d3.geoPath().projection(projection);
+
+            svg.selectAll(".subunit")
+                .data(regions.features)
+                .enter().append("path")
+                .attr("class", function(d) { return "region " + d.id; })
+                .attr("d", path);
+        });
     }
 
 }
 </script>
+
+<style>
+path {
+  fill: none;
+  stroke: #000;
+  stroke-width: 0.5px;
+}
+
+.region.GM0383 {
+    fill: lightgrey;
+}
+
+</style>
--- src/components/NetWorthVisualisationPage.vue
+++ src/components/NetWorthVisualisationPage.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="container mx-auto p-4">
     <h1 class="text-4xl">Net worth per province</h1>
+    <net-worth-map/>
   </div>
 </template>
 
 <script>
+import NetWorthMap from "./NetWorthMap.vue";
+
 export default {
   name: 'NetWorthVisualisationPage',
+  components: {
+    NetWorthMap
+  },
   props: {
   },
   data() {
