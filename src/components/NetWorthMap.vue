f9c6298719b97d6270ca5943c9b4667d401b18b9
Daan Oudejans <D.A.J.Oudejans@student.tudelft.nl>
2020-12-03 10:49:05 +0100

    Add buttons for switching map type

--- src/components/NetWorthMap.vue
+++ src/components/NetWorthMap.vue
@@ -8,35 +8,51 @@ import * as d3 from "d3";
 export default {
     name: 'NetWorthMap',
     props: {
+        municipalityMap: Boolean
     },
     data() {
         return {
         }
     },
-    async mounted() {
-        console.log('hallo');
-        const width = 960,
+    methods: {
+        async initMap(municipalityMap) {
+            const width = 960,
             height = 1160;
 
-        const svg = d3.select("body").append("svg")
-            .attr("width", width)
-            .attr("height", height);
+            const svg = d3.select("#map").append("svg")
+                .attr("width", width)
+                .attr("height", height);
 
-        const geoRegions = await d3.json("gemeente_2020.geojson");
+            var geoRegions;
+            if (municipalityMap) {
+                geoRegions = await d3.json("gemeente_2020.geojson");
+            } else {
+                geoRegions = await d3.json("provincie_2020.geojson");
+            }
 
-        const projection = d3.geoMercator()
-            .scale(10000)
-            .center([0, 52])
-            .rotate([-4.8, 0])
-            .translate([width/2, height/2]);
+            const projection = d3.geoMercator()
+                .scale(10000)
+                .center([0, 52])
+                .rotate([-4.8, 0])
+                .translate([width/2, height/2]);
 
-        const path = d3.geoPath().projection(projection);
+            const path = d3.geoPath().projection(projection);
 
-        svg.selectAll(".region")
-            .data(geoRegions.features)
-            .enter().append("path")
-            .attr("class", function(d) { return "region " + d.id; })
-            .attr("d", path);
+            svg.selectAll(".region")
+                .data(geoRegions.features)
+                .enter().append("path")
+                .attr("class", function(d) { return "region " + d.id; })
+                .attr("d", path);
+        }
+    },
+    async mounted() {
+        this.initMap(this.municipalityMap)
+    },
+    watch: {
+        municipalityMap: function() {
+            d3.select("svg").remove();
+            this.initMap(this.municipalityMap);
+        }
     }
 
 }
@@ -47,10 +63,6 @@ path {
   fill: none;
   stroke: #000;
   stroke-width: 0.5px;
-}
-
-.region.GM0383 {
-    fill: lightgrey;
 }
 
 </style>
--- src/components/NetWorthVisualisationPage.vue
+++ src/components/NetWorthVisualisationPage.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="container mx-auto p-4">
-    <h1 class="text-4xl">Net worth per municipality</h1>
-    <net-worth-map/>
+    <div class="flex-auto flex justify-between">
+      <h1 class="text-4xl">Net worth</h1>
+      <div class="flex">
+        <a class="button-group leftmost" :class="{ active: municipalityMap }" @click="municipalityMap = true">Municipalities</a>
+        <a class="button-group rightmost" :class="{ active: !municipalityMap }" @click="municipalityMap = false">Provinces</a>
+      </div>
+    </div>
+
+    <net-worth-map v-bind:municipalityMap="municipalityMap"/>
   </div>
 </template>
 
@@ -17,7 +24,26 @@ export default {
   },
   data() {
     return {
+      municipalityMap: true
     }
   }
 }
 </script>
+
+<style>
+.button-group {
+  @apply relative inline-flex items-center px-2 border border-blue-800 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 cursor-pointer select-none duration-200 ease-in-out;
+}
+
+.button-group.leftmost {
+  @apply rounded-l-md;
+}
+
+.button-group.rightmost {
+  @apply rounded-r-md;
+}
+
+.button-group.active {
+  @apply bg-blue-800 text-white hover:bg-blue-700;
+}
+</style>
