90bd632c009a8cdba6b1336c2d68c60ff789eb6e
Daan Oudejans <D.A.J.Oudejans@student.tudelft.nl>
2020-12-10 17:38:47 +0100

    Move data import and add DetailPlot component

---
+++ src/components/DetailPlot.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="plot"></div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+    name: 'DetailPlot',
+    props: {
+        activeStatistic: String,
+        activeFeature: String,
+        activeYear: Number
+    },
+    data() {
+        return {
+        }
+    },
+    computed: {
+    },
+    methods: {
+    },
+    async mounted() {
+
+    }
+}
+</script>
+
--- src/components/NetWorthVisualisationPage.vue
+++ src/components/NetWorthVisualisationPage.vue
@@ -2,34 +2,58 @@
   <div class="container mx-auto p-4 h-screen flex flex-col">
     <h1 class="text-4xl font-serif">Household wealth distribution</h1>
 
-    <div class="container mx-auto flex-1 flex">
-      <net-worth-map class="flex w-2/3 mt-2 mr-2 mb-2 max-h-screen"
-        v-bind:municipalityMap="municipalityMap"
-        v-bind:activeStatistic="activeStatistic"
-        v-bind:activeFeature="activeFeature"
-        v-bind:activeYear="activeYear"/>
-      <sidebar class="w-1/3 mt-2 ml-2 mb-2"
-        v-bind:municipalityMap="municipalityMap"
-        v-bind:activeStatistic="activeStatistic"
-        v-bind:activeFeature="activeFeature"
-        v-bind:activeYear="activeYear"
-        @switchMap="switchMap"
-        @switchStatistic="switchStatistic"
-        @switchFeature="switchFeature"
-        @switchYear="switchYear"/>
+    <div class="container mx-auto flex-1 flex mt-2">
+      <net-worth-map v-if="provinceRegions" class="flex w-2/3 mr-2 mb-2 max-h-screen"
+        :municipalityMap="municipalityMap"
+        :activeStatistic="activeStatistic"
+        :activeFeature="activeFeature"
+        :activeYear="activeYear"
+        :wealthMunicipalities="wealthMunicipalities"
+        :groupedFeaturesMunicipalities="groupedFeaturesMunicipalities"
+        :wealthProvinces="wealthProvinces"
+        :groupedFeaturesProvinces="groupedFeaturesProvinces"
+        :municipalityRegions="municipalityRegions"
+        :provinceRegions="provinceRegions"/>
+
+      <div class="container w-1/3 ml-2 mb-2">
+        <detail-plot v-if="provinceRegions" class="flex w-full"
+          :activeFeature="activeFeature"
+          :activeStatistic="activeStatistic"
+          :activeYear="activeYear"
+          :wealthMunicipalities="wealthMunicipalities"
+          :groupedFeaturesMunicipalities="groupedFeaturesMunicipalities"
+          :wealthProvinces="wealthProvinces"
+          :groupedFeaturesProvinces="groupedFeaturesProvinces"
+          :municipalityRegions="municipalityRegions"
+          :provinceRegions="provinceRegions"/>
+
+        <sidebar class="mt-2 w-full"
+          :municipalityMap="municipalityMap"
+          :activeStatistic="activeStatistic"
+          :activeFeature="activeFeature"
+          :activeYear="activeYear"
+          @switchMap="switchMap"
+          @switchStatistic="switchStatistic"
+          @switchFeature="switchFeature"
+          @switchYear="switchYear"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import * as d3 from "d3";
+import {groupBy} from "lodash";
 import NetWorthMap from "./NetWorthMap.vue";
 import Sidebar from "./Sidebar.vue";
+import DetailPlot from "./DetailPlot.vue";
 
 export default {
   name: 'NetWorthVisualisationPage',
   components: {
     NetWorthMap,
-    Sidebar
+    Sidebar,
+    DetailPlot
   },
   props: {
   },
@@ -38,8 +62,26 @@ export default {
       municipalityMap: true,
       activeStatistic: 'mean',
       activeFeature: '1050010',
-      activeYear: 2019
+      activeYear: 2019,
+      wealthMunicipalities: null,
+      groupedFeaturesMunicipalities: null,
+      wealthProvinces: null,
+      groupedFeaturesProvinces: null,
+      municipalityRegions: null,
+      provinceRegions: null,
     }
+  },
+  async mounted() {
+    const municipalityTable = await d3.csv('vermogen_gemeenten_modified.csv');
+      this.wealthMunicipalities = groupBy(municipalityTable, w => [w.Perioden, w.KenmerkenHuishouden]);
+      this.groupedFeaturesMunicipalities = groupBy(municipalityTable, w => [w.KenmerkenHuishouden]);
+
+      const provinceTable = await d3.csv('vermogen_provincies_modified.csv');
+      this.wealthProvinces = groupBy(provinceTable, w => [w.Perioden, w.KenmerkenHuishouden]);
+      this.groupedFeaturesProvinces = groupBy(provinceTable, w => [w.KenmerkenHuishouden]);
+
+      this.municipalityRegions = await d3.json("gemeente_2020.geojson");
+      this.provinceRegions = await d3.json("provincie_2020.geojson");
   },
   methods: {
     switchMap(newValue) {
