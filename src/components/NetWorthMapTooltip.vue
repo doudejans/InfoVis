3e75f1107e605ba0c7445a2e9451218750c87403
Martijn Comans <m.r.comans@student.tudelft.nl>
2020-12-10 17:45:20 +0100

    Add sparkline component

--- src/components/NetWorthMapTooltip.vue
+++ src/components/NetWorthMapTooltip.vue
@@ -1,23 +1,29 @@
 <template>
-    <div id="tooltip" class="absolute p-2 bg-white rounded border border-gray-200 border-solid shadow-md text-sm" :style="{left: mouseX + 'px', top: mouseY + 20 + 'px'}">
-        <p class="font-bold font-serif">
-            {{ regionName }}
-        </p>
-        <p class="font-light">
-            <span class="mr-2">{{valueDescription}}:</span>
-            <div v-if="value">
-                <span class="">{{value}}</span>
-                <span>{{valueUnit}} EUR</span>
-            </div>
-            <div class="" v-else>
-                No data available
-            </div>
-        </p>
+    <div id="tooltip" class="absolute p-2 bg-white rounded border border-gray-200 border-solid shadow-md text-sm flex items-center" :style="{left: mouseX + 'px', top: mouseY + 20 + 'px'}">
+        <div class="mr-2">
+            <p class="font-bold font-serif">
+                {{ regionName }}
+            </p>
+            <p class="font-light">
+                <span class="mr-2">{{valueDescription}}:</span>
+                <div v-if="value">
+                    <span class="">{{value}}</span>
+                    <span>{{valueUnit}} EUR</span>
+                </div>
+                <div class="" v-else>
+                    No data available
+                </div>
+            </p>
+        </div>
+        <sparkline width=100 height=50 />
     </div>
 </template>
 
 <script>
+import Sparkline from './Sparkline.vue';
+
 export default {
+    components: { Sparkline },
     name: 'NetWorthMapTooltip',
     props: {
         regionName: String,
---
+++ src/components/Sparkline.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg :width="width" :height="height" class="border"></svg>
+</template>
+
+<script>
+export default {
+    name: "Sparkline",
+    props: {
+        width: Number,
+        height: Number,
+    }
+}
+</script>
