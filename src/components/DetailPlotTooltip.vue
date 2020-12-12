d7522214472445cb1a02a98ef696907138eb81a7
Martijn Comans <m.r.comans@student.tudelft.nl>
2020-12-12 17:06:19 +0100

    Add tooltip to detail plot

---
+++ src/components/DetailPlotTooltip.vue
@@ -0,0 +1,20 @@
+<template>
+    <div 
+    class="absolute p-2 bg-white rounded border border-gray-200 border-solid shadow-md text-sm"
+    :style="{left: mouseX + 20 + 'px', top: mouseY + 'px'}">
+        <span class="">{{value}}</span>
+        <span class="ml-1 text-xs text-gray-400">{{valueUnit}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DetailPlotTooltip",
+    props: {
+        value: String,
+        valueUnit: String,
+        mouseX: Number,
+        mouseY: Number
+    }
+}
+</script>
